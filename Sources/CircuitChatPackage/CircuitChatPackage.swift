cd379a4d7323e2c4a49751c87ca8d03d8e129f38
Ranjit Singh <110404353+ranjit-ahead@users.noreply.github.com>
2023-12-14 14:25:36 +0530

    update

--- Sources/CircuitChatPackage/CircuitChatPackage.swift
+++ Sources/CircuitChatPackage/CircuitChatPackage.swift
@@ -1,6 +1,12 @@
+import SwiftUI
+
 public struct CircuitChatPackage {
     public private(set) var text = "Hello, World!"
-
+    
     public init() {
+    }
+    
+    public func createMainTabView() -> some View {
+        return MainTabView()
     }
 }
