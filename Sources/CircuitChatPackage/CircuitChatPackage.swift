0459148b4d00cd2a88417cb9878eafb097af84c4
Ranjit Singh <110404353+ranjit-ahead@users.noreply.github.com>
2023-12-15 13:02:26 +0530

    update

--- Sources/CircuitChatPackage/CircuitChatPackage.swift
+++ Sources/CircuitChatPackage/CircuitChatPackage.swift
@@ -6,6 +6,8 @@ public struct CircuitChatPackage {
     
     @ObservedObject private var observed = MainTabViewObserved()
     
+    @State private var originalNavBarAppearance: UINavigationBarAppearance?
+    
     public init(domain: String, userId: String, clientId: String, clientSecret: String) {
         circuitChatDomain = domain
         circuitChatUID = userId
@@ -17,6 +19,20 @@ public struct CircuitChatPackage {
     public func createMainTabView() -> some View {
         return MainTabView(observed: observed)
             .environmentObject(socketIO)
+            .onAppear {
+                // Save the original appearance when the view appears
+                originalNavBarAppearance = UINavigationBar.appearance().standardAppearance
+                
+                // Change to the default appearance when the view appears
+                let defaultAppearance = UINavigationBarAppearance()
+                UINavigationBar.appearance().standardAppearance = defaultAppearance
+                UINavigationBar.appearance().scrollEdgeAppearance = defaultAppearance
+            }
+            .onDisappear {
+                // Save the original appearance when the view appears
+                UINavigationBar.appearance().standardAppearance = originalNavBarAppearance!
+                UINavigationBar.appearance().scrollEdgeAppearance = originalNavBarAppearance!
+            }
     }
 
     // Function to handle login
